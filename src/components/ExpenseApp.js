6fa44a74da2ab8d243a5348849619c99e12ab500
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-09-27 00:09:25 +0330

    expense app file with overview and transaction

---
+++ src/components/ExpenseApp.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Overview from "./OverviewComp";
+import TransactionComponent from "./TransactionComp";
+
+const ExpenseApp = () => {
+
+    const [income, setIncome] = useState(0)
+    const [expense, setExpense] = useState(0)
+    const [transaction, setTransaction] = useState([])
+
+    const addTransaction = (values) => {
+        console.log(values);
+        const data = { ...values, id: Date.now() }
+        if (data.type === "expense") {
+            setExpense(expense + parseFloat(data.amount))
+        }
+        else {
+            setIncome(income + parseFloat(data.amount))
+        }
+        setTransaction([...transaction, data])
+    }
+
+    return (
+        <section className="container">
+            <Overview
+                income={income}
+                expense={expense}
+                addTransaction={addTransaction}
+            />
+            <TransactionComponent transactions={transaction} />
+        </section>
+    );
+}
+
+export default ExpenseApp;
